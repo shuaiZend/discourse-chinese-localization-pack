cfa60b8ab306df381468c2f1b1561e09f8137d0c
shuaiZend <shuai.zend@qq.com>
2017-08-25 15:21:20 +0800

    Update devdoc_onebox_spec.rb

--- spec/components/onebox/devdoc_onebox_spec.rb
+++ spec/components/onebox/devdoc_onebox_spec.rb
@@ -1,8 +1,9 @@
 require 'spec_helper'
 load File.expand_path('../../../helpers.rb', __FILE__)
-
 describe Onebox::Engine::DevdocOnebox do
+  let(:link) { "https://dov.magentochina.org" }
+  let(:html) { described_class.new(link).to_html }
   before do
-    FakeWeb.register_uri(:get, 'https://doc.magentochina.org/guides/v2.1/install-gde/install-quick-ref.html', body: response('Devdoc'), content_type: 'text/html')
+    fake(link, response("devdoc.response"))
   end
 end
